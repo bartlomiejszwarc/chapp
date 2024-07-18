a88f96fb729b51bfa626bf018cf0c56c99ead69c
bartlomiejszwarc <szwarc.it@gmail.com>
2024-07-18 14:10:44 +0200

    sign up form

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,16 @@
+import SignUpForm from '@/components/form/sign-up-form';
+
 export default function Page() {
-  return <span>xd</span>;
+  return (
+    <div className='min-h-screen w-full  flex'>
+      <div className='w-0 lg:w-1/2 xl:w-2/3 bg-sky-200 dark:bg-zinc-800 flex text-zinc-800 '>
+        <span>Left - image</span>
+      </div>
+      <div className='w-full lg:w-1/2 xl:w-1/3 bg-white dark:bg-zinc-900 flex items-center justify-center'>
+        <div className='w-[90%] sm:w-[60%] lg:w-[80%]'>
+          <SignUpForm />
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/form/sign-up-form.tsx
+++ components/form/sign-up-form.tsx
@@ -1,44 +1,50 @@
 'use client';
 import {useReducer, useState} from 'react';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import LockIcon from '@mui/icons-material/Lock';
+import FormSubmitButton from './form-submit-button';
+import Link from 'next/link';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import Link from 'next/link';
 import InputWithIconTemplate from './input-with-icon-template';
-import FormSubmitButton from './form-submit-button';
-interface SignInFields {
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LockIcon from '@mui/icons-material/Lock';
+import BadgeIcon from '@mui/icons-material/Badge';
+
+interface SignUpFields {
   email: string;
+  name: string;
   password: string;
+  repeatPassword: string;
 }
 interface Action {
   type: string;
   payload: string;
 }
-
-export default function SignInForm() {
-  const initialFormState: SignInFields = {email: '', password: ''};
-  const reducer = (state: SignInFields, action: Action) => {
+export default function SignUpForm() {
+  const initialFormState: SignUpFields = {email: '', name: '', password: '', repeatPassword: ''};
+  const reducer = (state: SignUpFields, action: Action) => {
     switch (action.type) {
       case 'SET_EMAIL':
         return {...state, email: action.payload};
+      case 'SET_NAME':
+        return {...state, name: action.payload};
       case 'SET_PASSWORD':
         return {...state, password: action.payload};
+      case 'SET_REPEAT_PASSWORD':
+        return {...state, repeatPassword: action.payload};
       default:
         return state;
     }
   };
   const [state, dispatch] = useReducer(reducer, initialFormState);
   const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
-  const handleSignIn = () => {
+  const handleSignUp = () => {
     console.log(state);
   };
-
   return (
     <div className='w-full'>
       <div className='bg-transparent rounded-lg flex flex-col px-3'>
-        <span className='text-zinc-800 dark:text-zinc-100 font-semibold text-3xl'>Welcome back!</span>
-        <span className='text-zinc-500 text-lg'>Please sign in to your account.</span>
+        <span className='text-zinc-800 dark:text-zinc-100 font-semibold text-3xl'>Join our community!</span>
+        <span className='text-zinc-500 text-lg'>Create an account to get started.</span>
         <div className='flex flex-col space-y-6 py-8'>
           <InputWithIconTemplate icon={<MailOutlineIcon className='text-zinc-50 dark:text-zinc-400' />}>
             <input
@@ -47,6 +53,16 @@ export default function SignInForm() {
               type='email'
               onChange={(e) => {
                 dispatch({type: 'SET_EMAIL', payload: e.target.value});
+              }}
+            />{' '}
+          </InputWithIconTemplate>
+          <InputWithIconTemplate icon={<BadgeIcon className='text-zinc-50 dark:text-zinc-400' />}>
+            <input
+              className='w-full h-full outline-none bg-transparent text-zinc-800 dark:text-zinc-300 dark:placeholder:text-zinc-500'
+              placeholder='Your name'
+              type='text'
+              onChange={(e) => {
+                dispatch({type: 'SET_NAME', payload: e.target.value});
               }}
             />{' '}
           </InputWithIconTemplate>
@@ -67,16 +83,33 @@ export default function SignInForm() {
               )}
             </button>
           </InputWithIconTemplate>
+          <InputWithIconTemplate icon={<LockIcon className='text-zinc-50 dark:text-zinc-400' />}>
+            <input
+              className='w-full h-full outline-none bg-transparent text-zinc-800 dark:text-zinc-300 dark:placeholder:text-zinc-500'
+              placeholder='Repeat password'
+              type={`${passwordVisible ? 'text' : 'password'}`}
+              onChange={(e) => {
+                dispatch({type: 'SET_REPEAT_PASSWORD', payload: e.target.value});
+              }}
+            />
+            <button className='pr-2 flex items-center' onClick={() => setPasswordVisible(!passwordVisible)}>
+              {!passwordVisible ? (
+                <VisibilityOffIcon className='text-zinc-400 text-xl' />
+              ) : (
+                <VisibilityIcon className='text-zinc-400 text-xl' />
+              )}
+            </button>
+          </InputWithIconTemplate>
         </div>
       </div>
       <div className='flex justify-center space-y-2 px-3 pb-4'>
-        <FormSubmitButton text={'Sign in'} onClick={handleSignIn} />
+        <FormSubmitButton text={'Sign in'} onClick={handleSignUp} />
       </div>
       <div className='px-3'>
         <span className='dark:font-thin text-zinc-700 dark:text-zinc-300'>
-          Don't have an account yet?{' '}
-          <Link href='/signup' className='text-sky-500 font-semibold dark:font-medium'>
-            Sign up here!
+          Already have an account?{' '}
+          <Link href='/login' className='text-sky-500 font-semibold dark:font-medium'>
+            Sign in
           </Link>
         </span>
       </div>
